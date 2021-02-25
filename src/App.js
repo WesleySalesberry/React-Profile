934bdbe17c115e1213a04a2e914cd711060060f8
Wes <wesleySalesberry@hotmail.com>
2021-02-25 13:05:32 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { AppStyle, MainContent, Content, Sidebar, NavBTN } from './Styles/layout'
+import { Route, Switch } from 'react-router-dom';
+import {NavBar} from './Component/NavBar'
+import { HomePage } from './Pages/HomePage';
+import { AboutPage } from './Pages/AboutPage';
+import { ProjectsPage } from './Pages/ProjectsPage';
+
+
+
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+
+  const navClick = () =>{
+    setToggle(!toggle)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppStyle className="App">
+      <Sidebar>
+        <NavBar />
+      </Sidebar>
+      <NavBTN onClick={navClick}>
+        <div className="lines-1"></div>
+        <div className="lines-2"></div>
+        <div className="lines-3"></div>
+      </NavBTN>
+      <MainContent>
+        <Content>
+          <Switch>
+            <Route exact path="/" component={HomePage}/>
+            <Route exact path="/about" component={AboutPage}/>
+            <Route exact path="/projects" component={ProjectsPage}/>
+          </Switch>
+        </Content>
+      </MainContent>
+    </AppStyle>
   );
 }
 
