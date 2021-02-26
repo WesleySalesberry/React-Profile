90cf723794e17f8d21d8078354bdb8464bed3181
Wes <wesleySalesberry@hotmail.com>
2021-02-26 10:43:00 -0500

    added a conditonal

--- src/Component/ProjectComponent.js
+++ src/Component/ProjectComponent.js
@@ -1,13 +1,32 @@
 import React from 'react'
+import { ProjectsContainer, Container,  LinkContainer, A } from '../Styles/projects'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+
 
 export const ProjectComponent = ({ projects }) => {
     return (
-        <div>
+        <ProjectsContainer>
             {
-                projects.map(item => {
-                    console.log(item)
-                })
+                 projects && projects.map((item) => (
+                     <Container key={item.id}>
+                         <h5>{item.name}</h5>
+                         <p>{item.description}</p>
+                         <p>{item.languages}</p>
+                         <LinkContainer>
+                             {
+                                 item.github ? <A href={item.github} target="_blank" rel="noopener noreferrer" className="github-icon" > <FontAwesomeIcon icon={faGithub} className="github-icon"/> Github</A> : ''
+                             }
+                             {
+                                 item.website ? <A href={item.website} target="_blank" rel="noopener noreferrer" className="link-icon"><FontAwesomeIcon icon={faExternalLinkAlt} className="link-icon"/> Website</A> : ''
+                             }                             
+                         </LinkContainer>
+                     </Container> 
+                ))
             }
-        </div>
+        </ProjectsContainer>
     )
 }
