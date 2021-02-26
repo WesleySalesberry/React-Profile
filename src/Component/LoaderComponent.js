390b456602c2c6da1e8274ecb7dc70b2a03f8869
Wes <wesleySalesberry@hotmail.com>
2021-02-26 10:40:50 -0500

    inital commit for layout for loader

---
+++ src/Component/LoaderComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { LoaderContainer, AnminatedLoader } from '../Styles/loadersStyle'
+
+export const LoaderComponent = () => {
+    return (
+        <LoaderContainer>
+           <div>
+               <h1>Loading...</h1>
+           </div>
+        </LoaderContainer>
+    )
+}
