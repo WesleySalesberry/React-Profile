98de486d8ecf85c2a7eb0c0e37ae565e6a1c96c0
Wes <wesleySalesberry@hotmail.com>
2021-02-26 10:55:11 -0500

    inital layout

---
+++ src/Styles/loadersStyle.js
@@ -0,0 +1,15 @@
+import style, {keyframes} from 'styled-components';
+
+export const LoaderContainer = style.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 250px
+`
+
+
+
+
+
+
+
