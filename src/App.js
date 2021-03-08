151092814a08840d65d6da41b7666f66d49a117b
Wes <wesleySalesberry@hotmail.com>
2021-03-08 13:01:26 -0500

    added routes and adjusted items

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+import React from 'react'
 import './App.css';
 import { useState } from 'react';
 import { AppStyle, MainContent, Content, Sidebar, NavBTN } from './Styles/layout'
 import { Route, Switch } from 'react-router-dom';
-import {NavBar} from './Component/NavBar'
+import NavBar from './Component/NavBar'
 import { HomePage } from './Pages/HomePage';
 import { AboutPage } from './Pages/AboutPage';
 import { ProjectsPage } from './Pages/ProjectsPage';
 import { BlogsPage } from './Pages/BlogsPage';
 import { BlogPostPage } from './Pages/BlogPostPage';
-
-
-
+import { Page404 } from './Pages/Page404';
+import { LoginPage } from './Pages/LoginPage';
+import { DashboardPage } from './Pages/AuthenticatedPages/DashboardPage';
+import { UnpubBlogs } from './Pages/AuthenticatedPages/UnpubBlogs';
 
 function App() {
   const [toggle, setToggle] = useState(false);
@@ -32,11 +34,17 @@ function App() {
       <MainContent>
         <Content>
           <Switch>
+            
             <Route exact path="/" component={HomePage}/>
             <Route exact path="/about" component={AboutPage}/>
             <Route exact path="/projects" component={ProjectsPage}/>
             <Route exact path="/blog" component={BlogsPage}/>
             <Route exact path="/blog-post/:id" component={BlogPostPage}/>
+            <Route exact path="/admin" component={LoginPage}/>
+            {/* TODO: make these private routes */}
+            <Route exact path="/dashboard" component={DashboardPage}/>
+            <Route exact path="/unpublished-blogs" component={UnpubBlogs}/>
+            <Route path="*" component={Page404}/>
           </Switch>
         </Content>
       </MainContent>
